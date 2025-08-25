cf04de53a64b049bbc7c20df2b8bae384ebb6354
haloperidozz <196709604+haloperidozz@users.noreply.github.com>
2025-08-25 19:52:02 +0600

    fix: my attempts to fix cursor positioning with uinput

--- src/mouse/gnulinux/UinputMouseInputBackend.cxx
+++ src/mouse/gnulinux/UinputMouseInputBackend.cxx
@@ -20,6 +20,7 @@
 #include <cstring>
 
 #include <linux/uinput.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -32,10 +33,6 @@ UinputMouseInputBackend::UinputMouseInputBackend()
     constexpr auto UinputPath{"/dev/uinput"};
     constexpr auto DeviceName{"wxclicker-uinput"};
 
-    // TODO: Make a screen resolution determination (!!!!!)
-    constexpr auto ScreenWidth{1920};
-    constexpr auto ScreenHeight{1080};
-
     _fd = ::open(UinputPath, O_WRONLY | O_NONBLOCK);
 
     if (_fd < 0) {
@@ -47,21 +44,9 @@ UinputMouseInputBackend::UinputMouseInputBackend()
     ::ioctl(_fd, UI_SET_KEYBIT, BTN_RIGHT);
     ::ioctl(_fd, UI_SET_KEYBIT, BTN_MIDDLE);
 
-    ::ioctl(_fd, UI_SET_EVBIT, EV_ABS);
-    ::ioctl(_fd, UI_SET_ABSBIT, ABS_X);
-    ::ioctl(_fd, UI_SET_ABSBIT, ABS_Y);
-
-    ::ioctl(_fd, UI_SET_EVBIT, EV_SYN);
-
-    uinput_abs_setup uabssetup{};
-
-    uabssetup.code = ABS_X;
-    uabssetup.absinfo.maximum = ScreenWidth - 1;
-    ::ioctl(_fd, UI_ABS_SETUP, &uabssetup);
-
-    uabssetup.code = ABS_Y;
-    uabssetup.absinfo.maximum = ScreenHeight - 1;
-    ::ioctl(_fd, UI_ABS_SETUP, &uabssetup);
+    ::ioctl(_fd, UI_SET_EVBIT, EV_REL);
+    ::ioctl(_fd, UI_SET_RELBIT, REL_X);
+    ::ioctl(_fd, UI_SET_RELBIT, REL_Y);
 
     uinput_setup usetup{};
 
@@ -70,6 +55,7 @@ UinputMouseInputBackend::UinputMouseInputBackend()
     usetup.id.bustype = BUS_USB;
     usetup.id.vendor  = 0x1234; // Randomize?
     usetup.id.product = 0x5678;
+    usetup.id.version = 1;
 
     if (::ioctl(_fd, UI_DEV_SETUP, &usetup) < 0) {
         throw std::runtime_error("Failed to setup device");
@@ -119,14 +105,20 @@ void UinputMouseInputBackend::Up(MouseButton button) noexcept
 
 void UinputMouseInputBackend::SetPosition(int x, int y) noexcept
 {
-    Emit(EV_ABS, ABS_X, x);
-    Emit(EV_ABS, ABS_Y, y);
+    Emit(EV_REL, REL_X, INT32_MIN);
+    Emit(EV_REL, REL_Y, INT32_MIN);
+    Emit(EV_SYN, SYN_REPORT, 0);
+
+    Emit(EV_REL, REL_X, x);
+    Emit(EV_REL, REL_Y, y);
     Emit(EV_SYN, SYN_REPORT, 0);
 }
 
 void UinputMouseInputBackend::Emit(int type, int code, int val) noexcept
 {
     input_event event{};
+
+    ::gettimeofday(&event.time, nullptr);
 
     event.type = type;
     event.code = code;
